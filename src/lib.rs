d93a7cf6b09e99b26a9f61c6499e1c970e6cb9ed
summivox <summivox@gmail.com>
2018-04-02 15:13:57 -0700

    init (1D slice)

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+extern crate num_traits;
+
+#[cfg(test)]
+extern crate rand;
+
+mod lowbit;
+pub mod slice;
+// pub mod bit2d;
---
+++ src/slice.rs
@@ -0,0 +1,65 @@
+use std::ops::AddAssign;
+use lowbit::lowbit;
+
+pub fn add<T>(fenwick: &mut [T], i: usize, delta: T)
+where
+    T: AddAssign + Copy,
+{
+    let n = fenwick.len();
+    let mut ii = i + 1;
+    while ii <= n {
+        fenwick[ii - 1] += delta;
+        ii += lowbit(ii);
+    }
+}
+
+pub fn partial_sum<T>(fenwick: &[T], i: usize) -> T
+where
+    T: AddAssign + Copy + Default,
+{
+    let mut ret = T::default();
+    let mut ii = i + 1;
+    while ii != 0 {
+        ret += fenwick[ii - 1];
+        ii -= lowbit(ii);
+    }
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::AddAssign;
+    use rand::{thread_rng, Rng, distributions::{Distribution, Range}};
+
+    fn partial_sum_scanner<T>(s: &mut T, x: &T) -> Option<T>
+    where
+        T: AddAssign + Copy,
+    {
+        *s += *x;
+        Some(*s)
+    }
+
+    fn random_one<TRng: Rng>(rng: &mut TRng, len: usize) {
+        let mut data = vec![0i32; len];
+        let range = Range::new_inclusive(-50, 50);
+        for x in data.iter_mut() {
+            *x = range.sample(rng);
+        }
+        let psum: Vec<i32> = data.iter().scan(0i32, partial_sum_scanner).collect();
+        let mut fenwick = vec![0i32; data.len()];
+        for (i, x) in data.iter().enumerate() {
+            super::add(&mut fenwick[..], i, *x);
+        }
+        for (i, s) in psum.iter().enumerate() {
+            assert_eq!(super::partial_sum(&fenwick[..], i), *s);
+        }
+    }
+
+    #[test]
+    fn randoms() {
+        let mut rng = thread_rng();
+        for len in 0..130usize {
+            random_one(&mut rng, len);
+        }
+    }
+}
